107999c6e5f79a75b466af9d54000febeaa1a65e
cloudyan <1395093509@qq.com>
2014-12-18 20:23:53 +0800

    二次开发修改

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+
 var routes = require('./routes/index');
+var settings = require('./settings');
+
+var flash = require('connect-flash');
 //var users = require('./routes/users');
 
 var app = express();
@@ -15,18 +21,39 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: settings.cookieSecret,
+  key: settings.db,//cookie name
+  cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},//30 days
+  store: new MongoStore({
+    db: settings.db,
+    host: settings.host,
+    port: settings.port
+  })
+}));
+
+app.use(flash());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 
 routes(app);
 // app.use('/', routes);
 // app.use('/users', users);
+
+
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -60,4 +87,4 @@ app.use(function(err, req, res, next) {
 });
 
 
-module.exports = app;
+//module.exports = app;
