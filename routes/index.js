6ccd35dcd379d31e7c1b9a0ce24f3d3827b20080
cloudyan <1395093509@qq.com>
2015-03-12 11:00:37 +0800

    更新到第七章

--- routes/index.js
+++ routes/index.js
@@ -9,14 +9,20 @@ var router = express.Router();
 /* GET home page. */
 module.exports = function(app) {
   app.get('/', function (req, res) {
-    Post.getAll(null, function (err, posts) {
+    //判断是否是第一页，并把请求的页数转换成 number 类型
+    var page = req.query.p ? parseInt(req.query.p) : 1;
+    //查询并返回第 page 页的 5 篇文章
+    Post.getByPage(null, page, function (err, posts, total) {
       if (err) {
         posts = [];
       } 
       res.render('index', {
         title: '主页',
-        user: req.session.user,
         posts: posts,
+        page: page,
+        isFirstPage: (page - 1) == 0,
+        isLastPage: ((page - 1) * 5 + posts.length) == total,
+        user: req.session.user,
         success: req.flash('success').toString(),
         error: req.flash('error').toString()
       });
@@ -154,8 +160,10 @@ module.exports = function(app) {
         req.flash('error', '用户不存在!'); 
         return res.redirect('/');//用户不存在则跳转到主页
       }
-      //查询并返回该用户的所有文章
-      Post.getAll(user.name, function (err, posts) {
+      //判断是否是第一页，并把请求的页数转换成 number 类型
+      var page = req.query.p ? parseInt(req.query.p) : 1;
+      //查询并返回该用户第 page 页的 5 篇文章
+      Post.getByPage(user.name, page, function (err, posts, total) {
         if (err) {
           req.flash('error', err); 
           return res.redirect('/');
@@ -163,6 +171,9 @@ module.exports = function(app) {
         res.render('user', {
           title: user.name,
           posts: posts,
+          page: page,
+          isFirstPage: (page - 1) == 0,
+          isLastPage: ((page - 1) * 5 + posts.length) == total,
           user : req.session.user,
           success : req.flash('success').toString(),
           error : req.flash('error').toString()
