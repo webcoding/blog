34e6ccb86d97c9a093672edc2e44b6211ee47622
cloudyan <1395093509@qq.com>
2015-03-12 18:08:22 +0800

    第11章 增加文章检索功能

--- models/post.js
+++ models/post.js
@@ -338,8 +338,6 @@ Post.update = function(name, day, title, post, callback) {
   });
 };
 
-
-
 //返回所有文章存档信息
 Post.getArchive = function(callback) {
   //打开数据库
@@ -452,6 +450,38 @@ Post.getTag = function(tag, callback) {
         mongodb.close();
         if (err) {
           return callback(err);
+        }
+        callback(null, docs);
+      });
+    });
+  });
+};
+
+
+//返回通过标题关键字查询的所有文章信息
+Post.search = function(keyword, callback) {
+  mongodb.open(function (err, db) {
+    if (err) {
+      return callback(err);
+    }
+    db.collection('posts', function (err, collection) {
+      if (err) {
+        mongodb.close();
+        return callback(err);
+      }
+      var pattern = new RegExp(keyword, "i");
+      collection.find({
+        "title": pattern
+      }, {
+        "name": 1,
+        "time": 1,
+        "title": 1
+      }).sort({
+        time: -1
+      }).toArray(function (err, docs) {
+        mongodb.close();
+        if (err) {
+         return callback(err);
         }
         callback(null, docs);
       });
